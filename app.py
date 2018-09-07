b1974e6ac71763a2ae2d83312704ded63b55d848
Cesar Ramos <cer2178@columbia.edu>
2018-09-07 16:30:03 -0400

    Finished app.py. Added id.html and name.html to templates

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
 
+@app.route("/pokemon/<endpoint>", methods=['GET'])
+def pokemon_display(endpoint):
+	url = 'http://pokeapi.co/api/v2/pokemon/'+endpoint
+	r = requests.get(url)
+	json_response = r.json()
 
-@app.route('/', methods=['GET'])
-def main():
-    return render_template('index.html')
+	name = json_response['name'].capitalize()
+	id = str(json_response['id'])
 
+	if endpoint.isdigit():
+		return render_template('name.html', name=name,id=id)
+	else:
+		return render_template('id.html', name=name,id=id)
+		
 
 if __name__ == '__main__':
     app.run()
